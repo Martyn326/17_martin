f55542fd523eaeb7231da7dca3b54eb8c44ddfc6
martin.mihailov <martin.mihailov@educa.madrid.org>
2025-12-05 14:17:48 +0100

    TELKWGF

---
+++ 4/prueba1.js
@@ -0,0 +1,16 @@
+// soy un comentario de una linea
+        /* j------------------ */
+        //alert("prueba de codigo");
+        var mensaje = "hola mundi"
+        console.log(mensaje)
+        console.log(mensaje)
+        console.log(localStorage)
+var esViernes = true;
+console.log("Es viernes y queremos irnos: " + esViernes);
+debugger
+if(esViernes == true){
+        console.log("Pues si que es viernes , nos quedan 45 minutos para irnos ");
+        }
+var nombreUsuario = prompt("INtroduccion")
+console.log("El combre de usuario " , nombreUsuario);
+document.write("<h1>" + nombreUsuario + "</h1>");
